1dc5cc3b22765b306f2d73dbe2b47c99748f6140
Dwight Carter <carter91jr@gmail.com>
2018-07-20 11:29:17 -0400

    added player movement

--- js/app.js
+++ js/app.js
@@ -6,11 +6,13 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -18,15 +20,48 @@ Enemy.prototype.update = function(dt) {
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
+    
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Hero {
+    constructor(){
+        this.sprite ='images/char-boy.png';
+        this.step = 101;
+        this.jump = 83;
+        this.startX = this.step * 2;
+        this.startY = this.jump * 5;
+        this.x = this.startX;
+        this.y = this.startY;
+    }
+    // Renders player image to page
+    render(){
+        ctx.drawImage(Resources.get(this.sprite),this.x, this.y);
+    }
+    handleInput(input){
+        switch(input){
+            case 'left':
+            this.x = this.step;
+            break;
+            case "up":
+            this.y -= this.jump;
+            break;
+            case "right":
+            this.x += this.step;
+            break;
+            case "down":
+            this.y += this.jump;
+            break;
 
+        }
+    }
+}
 
 // Now instantiate your objects.
+const player = new Hero;
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
